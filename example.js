5b535c3706b9fa4a4d7b2cafa1e29d2f054d9e57
politiz <rahulkhattri14@gmail.com>
2021-02-03 00:40:00 +0530

    started working on java script

---
+++ example.js
@@ -0,0 +1,19 @@
+console.log("hello world");
+alert("me");
+document.write("this is document write");
+console.warn("this is a warning");
+console.error("error");
+// javascript variables
+var number1 = 34;
+var number2 = 45;
+console.log(number1 + number2);
+// data types
+var str1= "this is a string";
+var str2= 'this is also a string';
+// objects
+var a= true;
+var b= false;
+console.log(a,b);
+var und;
+console.log(und)
+
