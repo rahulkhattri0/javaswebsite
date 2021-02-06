7454e6682b8375c398075ea76600290697eb0385
politiz <rahulkhattri14@gmail.com>
2021-02-06 12:39:57 +0530

    added some stuff

--- example.js
+++ example.js
@@ -26,4 +26,34 @@ var s=1;
 var r=1;
 console.log(s == r);
 console.log(true&&true);
-
+// functions
+function avg(a,b)
+{
+    return(a+b)/2;
+}
+c1= avg(4,6);
+console.log(c1);
+var age=36;
+if (age>18)
+{
+    console.log("you are not a kid");
+}
+else{
+    console.log("you are a kid");
+}
+for(var i=0;i<10;i++)
+{
+    if(i==5)
+    {
+        break;
+    }
+    console.log(i);
+}
+let arr = ['fan','camera',34,null,true];
+console.log(arr.length);
+arr.pop();
+arr.shift();
+console.log(arr);
+arr.toString();
+let date = new date();
+console.log(date);
